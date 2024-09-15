d529f53953d6c12b00bf812d0c31686b2a6d84c3
Pavel Sharko <pavel.sharko@mail.ru>
2024-09-15 22:58:12 +0300

    commit 15/09 deploy Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/coincap/', 
+  base: '/coincap/',
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+    'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
+  },
 });
+
+
